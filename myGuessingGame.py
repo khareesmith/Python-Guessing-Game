ebe9e07809a80731217390a066241ab5772e3fa4
khareesmith <kharee.smith@yahoo.com>
2020-08-10 20:05:59 -0400

    Third commit. Bug fixes and secret name input

--- myGuessingGame.py
+++ myGuessingGame.py
@@ -1,7 +1,8 @@
+#
 from random import *
 
-min = randint(0,10)
-max = randint(11,20)
+min = randrange(10)
+max = randrange(11,21)
 
 rand_num = randint(min,max)
 
@@ -10,20 +11,28 @@ user_name = raw_input("Input your name: ")
 
 number_of_guesses = 0
 
-print("\nGreetings " + user_name + "!\n\nI'm thinking of a number between " + str(min) + " and " + str(max) + ".\n")
+if (user_name == "Kharee"):
+    print("Greetings " + user_name + "! The secret number is " + str(rand_num) + ". ;)\n\nI'm thinking of a number between " + str(min) + " and " + str(max) + ".\n")
+else:    
+    print("\nGreetings " + user_name + "!\n\nI'm thinking of a number between " + str(min) + " and " + str(max) + ".\n")
 
-while number_of_guesses < 5:
-    print("You have " + str((5 - number_of_guesses)) + " guesses!")
-    user_guess = int(raw_input("Input a number: "))
+while number_of_guesses <= 5:
 
     if(number_of_guesses == 5):
-        print("Sorry, you're out of guesses! The number was: " + rand_num)
-    elif (user_guess < rand_num):
-        print("Too low, try again!")
+        print("Sorry, you're out of guesses! The number was: " + str(rand_num))
+        break
+
+    print("You have " + str(5 - (number_of_guesses)) + " guesses!")
+    user_guess = int(raw_input("Input a number: "))
+
+    
+    if (user_guess < rand_num):
+        print("You are too low!")
         number_of_guesses+=1
     elif(user_guess > rand_num):
-        print("Too high, try again!")
+        print("Oops too high!")
         number_of_guesses+=1
-    else:
-        print("You guessed correctly! Only took " + str(number_of_guesses) + " tries!")
+    elif(user_guess == rand_num):
+        print("You guessed correctly! Only took " + str(number_of_guesses + 1) + " tries!")
         break
+    
